c4a3c408a1899215c1a9e103e4950c3793188df3
“Akshay <akshay.sargar17@vit.edu>
2021-09-22 12:22:10 +0530

    Added eduflex-backend

---
+++ src/main/java/com/eduflex/eduflexbackend/EduflexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eduflex.eduflexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduflexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduflexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eduflex/eduflexbackend/EduflexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduflex.eduflexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduflexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
