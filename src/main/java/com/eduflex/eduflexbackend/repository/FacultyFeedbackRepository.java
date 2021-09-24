0752344eed27388179504d9875cad68efc6a4c1f
Akshay Sargar <akshay.sargar17@vit.edu>
2021-09-24 22:43:52 +0530

    Update FacultyFeedbackRepository.java

--- src/main/java/com/eduflex/eduflexbackend/repository/FacultyFeedbackRepository.java
+++ src/main/java/com/eduflex/eduflexbackend/repository/FacultyFeedbackRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eduflex.eduflexbackend.model.Faculty;
 import com.eduflex.eduflexbackend.model.FacultyFeedback;
 
-
-
 public interface FacultyFeedbackRepository extends JpaRepository<FacultyFeedback, Integer> {
    List<FacultyFeedback> findFacultyFeedbacksByFaculty(Faculty faculty);
 }
