4d05322e162e1610cb693cb33e28f186e7570634
Akshay Sargar <akshay.sargar17@vit.edu>
2021-09-24 11:58:21 +0530

    Update FacultyLeaveService.java

--- src/main/java/com/eduflex/eduflexbackend/service/FacultyLeaveService.java
+++ src/main/java/com/eduflex/eduflexbackend/service/FacultyLeaveService.java
@@ -5,11 +5,10 @@ import java.util.List;
 import com.eduflex.eduflexbackend.model.FacultyLeave;
 
 public interface FacultyLeaveService {
-	
 	List<FacultyLeave> getAllFacultyLeaves();
 	void deleteFacultyLeave(int facultyId,int facultyLeaveId);
 	List<FacultyLeave> getFacultyLeavesByFacultyId(int facultyId);
-    FacultyLeave getFacultyLeaveByFacultyLeaveId(int facultyId ,int facultyLeaveId);
+    	FacultyLeave getFacultyLeaveByFacultyLeaveId(int facultyId ,int facultyLeaveId);
 	FacultyLeave addFacultyLeaveToFaculty(int facultyId, FacultyLeave facultyLeave);
-    void deleteFacultyLeaveByFacultyLeaveID(int facultyId);
+    	void deleteFacultyLeaveByFacultyLeaveID(int facultyId);
 }
